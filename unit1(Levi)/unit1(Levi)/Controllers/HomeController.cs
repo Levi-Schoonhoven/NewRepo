54126bad7635a8aa931491073dcaa6de8d28da98
Levi-Schoonhoven <142834167+Levi-Schoonhoven@users.noreply.github.com>
2023-09-18 10:09:32 -0500

    Add files via upload

---
+++ unit1(Levi)/unit1(Levi)/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace unit1_Levi_.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel Model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = Model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(Model);
+
+        }
+
+    }
+}
---
+++ unit1(Levi)/unit1(Levi)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace unit1_Levi_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
